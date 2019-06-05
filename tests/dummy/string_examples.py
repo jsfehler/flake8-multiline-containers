2bdd620325e5e41a0f2ac0e79f2b740e64ed6c85
Joshua Fehler <jsfehler@gmail.com>
2019-06-05 11:57:25 -0400

    Ensure closing characters in strings are ignored

--- tests/dummy/string_examples.py
+++ tests/dummy/string_examples.py
@@ -1,2 +1,8 @@
 s = '{', '}', "foo={a\n", 'bar'
 s = '{', '}', "foo=}a\n", 'bar'
+
+string_only_opens_sq = '[foobar'
+string_only_closes_sq = 'foobar]'
+
+string_only_opens_dq = "[foobar"
+string_only_closes_dq = "foobar]"
