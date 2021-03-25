f6a9c8c390a9429f9668ddc200959304f8ec7c3f
Joshua Fehler <jsfehler@gmail.com>
2021-03-24 22:29:47 -0400

    fix unit test

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -2,10 +2,22 @@ from flake8_multiline_containers import ErrorCodes
 
 
 def test_check_opening_contains_error(linter):
-    linter._check_opening('{', '}', 0, "foo={a\n", ErrorCodes.JS101)
+    line = "foo={a\n"
+
+    curly_matches = linter._number_of_matches_in_line(
+        '{', '}', line,
+    )
+
+    linter._check_opening('{', '}', curly_matches, 0, line, ErrorCodes.JS101)
     assert 1 == len(linter.errors)
 
 
 def test_check_opening_no_error(linter):
-    linter._check_opening('{', '}', 0, "foo={\n", ErrorCodes.JS101)
+    line = "foo={\n"
+
+    curly_matches = linter._number_of_matches_in_line(
+        '{', '}', line,
+    )
+
+    linter._check_opening('{', '}', curly_matches, 0, line, ErrorCodes.JS101)
     assert 0 == len(linter.errors)
